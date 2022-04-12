dcdd5372848fed68441d46821aee23d314d774a2
srnyx <25808801+srnyx@users.noreply.github.com>
2022-04-12 14:31:42 -0400

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+description = "NAME"
+version = "0.0.1"
+group = "PATH"
+
+plugins {
+    id("java")
+}
+
+repositories {
+//    maven("LINK")
+}
+
+dependencies {
+//    compileOnly("PATH:NAME:VERSION")
+}
